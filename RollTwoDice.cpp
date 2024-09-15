c18e07e7fa6299cba802806c59f6869a83459bb3
Michael J <30031129@cc.peralta.edu>
2024-09-15 12:41:37 -0700

    added print output

--- RollTwoDice.cpp
+++ RollTwoDice.cpp
@@ -3,5 +3,10 @@
 #include <ctime>
 
 int main(){
-    
+    int maxValue = 6;
+    int minValue = 1;
+
+    srand(time(0));
+    std:: cout << (rand() %(maxValue - minValue +1)) + minValue << "\n";
+    std:: cout << (rand() %(maxValue - minValue +1)) + minValue << "\n";
 }
